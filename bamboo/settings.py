0bbfdcb5c4d1ba0d4e50fcf1a50c55f56fe03b15
MarsBighead <duanmhy@126.com>
2017-03-26 22:34:24 +0800

    Toger#Build application pms

--- bamboo/settings.py
+++ bamboo/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'pms',
 ]
 
 MIDDLEWARE = [
@@ -75,8 +76,12 @@ WSGI_APPLICATION = 'bamboo.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'dbsnp',
+        'USER':'hbu',
+        'PASSWORD':'Mars1234',
+        'HOST':'',
+        'PORT':'',
     }
 }
 
