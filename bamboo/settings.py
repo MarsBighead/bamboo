2bf618d13797b6f494e3a4b262fd4afe392b0f50
MarsBighead <duanmhy@126.com>
2017-03-28 23:25:55 +0800

    Toger#Build Django template

--- bamboo/settings.py
+++ bamboo/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'bootstrap_admin',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -55,7 +56,10 @@ ROOT_URLCONF = 'bamboo.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+
+             os.path.join(BASE_DIR, 'templates').replace('\\','/'),
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -123,3 +127,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = [
+   os.path.join(BASE_DIR,'/static').replace('\\','/'),
+]
--- pms/models.py
+++ pms/models.py
@@ -3,5 +3,13 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
-class Employee(models.Model):
-     name=models.CharField(max_length=20)
+class Report(models.Model):
+    name = models.CharField(max_length = 128)
+    keywords = models.CharField(max_length = 100, blank = True) # keyword tags 
+    date_time = models.DateTimeField(auto_now_add = True )
+    institution = models.CharField(max_length = 128)
+    content = models.TextField(blank = True, null = True)
+    def __unicode__(self):
+        return self.name
+    class Meta:
+        ordering = ['-date_time']  
