1bda246d8d271e2c8f54b3aea66263700a788410
MarsBighead <duanmhy@126.com>
2017-04-09 21:12:06 +0800

    Toger#Build first gentelella page with static files

--- bamboo/urls.py
+++ bamboo/urls.py
@@ -17,6 +17,7 @@ from django.conf.urls import url, include
 from django.contrib import admin
 from django.conf import settings
 from django.conf.urls.static import static
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns  
 from pms import views
 admin.autodiscover()
 
@@ -26,3 +27,4 @@ urlpatterns = [
     url(r'^pms/', include('pms.urls')),
 ]
 urlpatterns+=static(settings.STATIC_URL,document_root=settings.STATIC_ROOT)
+#urlpatterns += staticfiles_urlpatterns() 
