eedb61ca3faa13487d451cd6b032ff79ea1eddbc
MarsBighead <duanmhy@126.com>
2017-04-13 00:12:29 +0800

    Toger#Build all gentelella template for Django PMS

--- pms/views.py
+++ pms/views.py
@@ -31,6 +31,93 @@ def form_upload(request):
 def form_wizards(request):
     return render(request, "gentelella/form_wizards.htm", {'current_time':datetime.now()})
 
+def general_elements(request):
+    return render(request, "gentelella/general_elements.htm", {'current_time':datetime.now()})
+
+def icons(request):
+    return render(request, "gentelella/icons.htm", {'current_time':datetime.now()})
+
+def inbox(request):
+    return render(request, "gentelella/inbox.htm", {'current_time':datetime.now()})
+
+def e_commerce(request):
+    return render(request, "gentelella/e_commerce.htm", {'current_time':datetime.now()})
+
+def projects(request):
+    return render(request, "gentelella/projects.htm", {'current_time':datetime.now()})
+
+def project_detail(request):
+    return render(request, "gentelella/project_detail.htm", {'current_time':datetime.now()})
+
+def profile(request):
+    return render(request, "gentelella/profile.htm", {'current_time':datetime.now()})
+
+def contacts(request):
+    return render(request, "gentelella/contacts.htm", {'current_time':datetime.now()})
+
+def login(request):
+    return render(request, "gentelella/login.htm", {'current_time':datetime.now()})
+
+def media_gallery(request):
+    return render(request, "gentelella/media_gallery.htm", {'current_time':datetime.now()})
+
+def typography(request):
+    return render(request, "gentelella/typography.htm", {'current_time':datetime.now()})
+
+def calendar(request):
+    return render(request, "gentelella/calendar.htm", {'current_time':datetime.now()})
+
+def pricing_tables(request):
+    return render(request, "gentelella/pricing_tables.htm", {'current_time':datetime.now()})
+
+def level2(request):
+    return render(request, "gentelella/level2.htm", {'current_time':datetime.now()})
+
+def fixed_footer(request):
+    return render(request, "gentelella/fixed_footer.htm", {'current_time':datetime.now()})
+
+def fixed_sidebar(request):
+    return render(request, "gentelella/fixed_sidebar.htm", {'current_time':datetime.now()})
+
+def tables(request):
+    return render(request, "gentelella/tables.htm", {'current_time':datetime.now()})
+
+def tables_dynamic(request):
+    return render(request, "gentelella/tables_dynamic.htm", {'current_time':datetime.now()})
+
+def chartjs(request):
+    return render(request, "gentelella/chartjs.htm", {'current_time':datetime.now()})
+
+def chartjs2(request):
+    return render(request, "gentelella/chartjs2.htm", {'current_time':datetime.now()})
+
+def other_charts(request):
+    return render(request, "gentelella/other_charts.htm", {'current_time':datetime.now()})
+
+def morisjs(request):
+    return render(request, "gentelella/morisjs.htm", {'current_time':datetime.now()})
+
+def echarts(request):
+    return render(request, "gentelella/echarts.htm", {'current_time':datetime.now()})
+
+def widgets(request):
+    return render(request, "gentelella/widgets.htm", {'current_time':datetime.now()})
+
+def invoice(request):
+    return render(request, "gentelella/invoice.htm", {'current_time':datetime.now()})
+
+def glyphicons(request):
+    return render(request, "gentelella/glyphicons.htm", {'current_time':datetime.now()})
+
+def page_403(request):
+    return render(request, "gentelella/page_403.htm", {'current_time':datetime.now()})
+
+def page_404(request):
+    return render(request, "gentelella/page_404.htm", {'current_time':datetime.now()})
+
+def page_500(request):
+    return render(request, "gentelella/page_500.htm", {'current_time':datetime.now()})
+
 def detail(request, my_args):
     post = Article.objects.all()[int(my_args)]
     str =("name = %s, keywords = %s, date_time = %s, content = %s"
